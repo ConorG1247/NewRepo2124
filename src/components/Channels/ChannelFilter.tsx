ce7c97acb46568af55eef1ee744c8162f57603db
ConorG1247 <conorgoddard1247@gmail.com>
2022-10-05 18:48:32 +0100

    Working input check for if language exists

--- src/components/Channels/ChannelFilter.tsx
+++ src/components/Channels/ChannelFilter.tsx
@@ -3,36 +3,56 @@ import { useState } from "react";
 
 function ChannelFilter() {
   const [languageInput, setLanguageInput] = useState<string | undefined>();
+  const [noResultsCheck, setNoResultsCheck] = useState(false);
+
+  const languageResultsCheck = (input: string) => {
+    setLanguageInput(input);
+
+    if (
+      StreamLanguage.filter((lang) =>
+        lang.language.toLocaleLowerCase().includes(input)
+      ).length === 0
+    ) {
+      setNoResultsCheck(true);
+    } else {
+      setNoResultsCheck(false);
+    }
+  };
 
   return (
     <div className="channel-language-container">
       <div>Filter by Language: </div>
       <input
         className="channel-language-input"
-        onChange={(input) =>
-          setLanguageInput(input.target.value.toLocaleLowerCase())
-        }
+        onChange={(input) => {
+          languageResultsCheck(input.target.value.toLocaleLowerCase());
+        }}
       />
       <div className="channel-language-option-container">
         {StreamLanguage.map((language, index) => {
           return (
-            <div>
+            <div key={index}>
               {languageInput &&
                 language.language
                   .toLocaleLowerCase()
                   .includes(languageInput) && (
-                  <div key={index} className="channel-language-option">
+                  <div className="channel-language-option">
                     <div>{language.language}</div>
                   </div>
                 )}
               {!languageInput && (
-                <div key={index} className="channel-language-option">
+                <div className="channel-language-option">
                   <div>{language.language}</div>
                 </div>
               )}
             </div>
           );
         })}
+        {noResultsCheck && (
+          <div className="channel-language-option">
+            <div>No results found</div>
+          </div>
+        )}
       </div>
     </div>
   );
